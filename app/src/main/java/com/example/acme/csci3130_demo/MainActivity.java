65477bb0b786a4daf1135ca9d10b09fe54e2ebc4
Di Wu <dz247633@dal.ca>
2018-01-23 02:45:01 -0400

    second

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button b1;
+    EditText t1;
+    TextView o;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.b1);
+        t1 = findViewById(R.id.t1);
+        o =findViewById(R.id.origin);
+
+        b1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String temp = t1.getText().toString();
+        o.setText(temp);
     }
 }
